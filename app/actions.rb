a4e3fe734597edbe0af700ec16a81e2c600d2968
ForeverPlat <luqman.o.ajani@gmail.com>
2022-09-25 22:59:50 +0000

    Isolate HTML post content into Ruby hashes

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,27 @@
-get '/' do
-    File.read(File.join('app/views', 'index.html'))
+def humanized_time_ago(time_ago_in_minutes)
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} hours ago"
+  else
+    "#{time_ago_in_minutes} minutes ago"
   end
+end
+
+get '/' do
+
+
+  finstagram_post_shark = {
+    username: "sharky_j",
+    avatar_url: "https://live.staticflickr.com/65535/52358606250_01c667c5da_w.jpg",
+    photo_url: "https://live.staticflickr.com/65535/52358421508_786aa10e2c_c.jpg",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "sharky_j",
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    }]
+  }
+
+  [finstagram_post_shark].to_s
+
+end
